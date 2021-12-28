ebf983744ce113d6934956dd8be59c5aae14de36
Andres Bolivar <81452502+andressbolivarr@users.noreply.github.com>
2021-12-28 09:34:12 -0400

    deleted: unuseful files

--- P Scholarship/main.js
+++
@@ -1,6 +0,0 @@
-const burger = document.getElementById('burger')
-const navbar = document.getElementById('navbar')
-
-	burger.addEventListener('click', () =>{
-		navbar.classList.toggle('see');
-	});
